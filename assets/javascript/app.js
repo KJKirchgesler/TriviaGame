809503a8d5fc36f0ddb5cf517463ea9c2e846593
KRISTEN KIRCHGESLER <kristen@KRISTENs-MBP.attlocal.net>
2018-03-02 13:42:26 -0500

    updated pseudocode and js variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,34 @@
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
-var questionContent
-var answers
-var results
+//var selectedAnswers
+//var results
 
-var questions = [
-	["Which is the oldest operating airline in the world?", "Delta", "Aeroflot", "Avianca", "KLM", "D"],
-	["Which iconic aircraft was grounded for over a month in 1979 after a series of high-profile accidents?", "McDonnell Douglas DC-10", "Boeing 747", "Lockheed L-1011 TriStar", "Airbus 340"]
+//array holding the trivia questions
+var questionContent = [
+	["Which is the oldest operating airline in the world?"],
+	["Which iconic aircraft was grounded for over a month in 1979 after a series of high-profile accidents?"]
 	];
+
+//array holding the options to answer	
+var answerOptions = [
+	["Delta", "Aeroflot", "Avianca", "KLM"],
+	["McDonnell Douglas DC-10", "Boeing 747", "Lockheed L-1011 TriStar", "Airbus 340"]
+	];
+
+//array holding the correct answers
+var correctAnswers = ["KLM", "McDonnell Douglas DC-10"];
+
+//array holding the images for each answer
+var images = ["<img src='assets/images/giphy-klm.gif'>", "<img src='assets/images/aa-flight-191.jpg'>"];
 
 
 //Pseudocode
 //Show title screen with a start button
 //Click start button to begin game
-//Show one question at a time
-//Set timer for 45 seconds
-//Player can choose only 1 answer to each question 
+//Show first question
+//Set timer for 30 seconds
+//Player can choose only 1 answer the question 
 //Show question until
 	//Player answers or
 	//Time runs out
@@ -25,6 +37,8 @@ var questions = [
 	//if correct - say Congratulations! and show correct answer
 	//if incorrect - say Nope! and show correct answer
 	//if unanswered - say Time's up and show correct answer
+//Repeat process for show question, advance to feedback, and show feedback for the remaining questions
+//When last question is done advance to final screen
 //On final screen show restart button and 
 	//number of correct answers
 	//wrong answers 
