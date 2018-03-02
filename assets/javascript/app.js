9ae2ef831c27c3debb3c54d889821032f4aa6374
KRISTEN KIRCHGESLER <kristen@KRISTENs-MBP.attlocal.net>
2018-03-01 20:00:37 -0500

    added divs and buttons to index.html and question/answer array to app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+var questions = [
+	["Which is the oldest operating airline in the world?", "Delta", "Aeroflot", "Avianca", "KLM", "D"],
+	["Which iconic aircraft was grounded for over a month in 1979 after a series of high-profile accidents?", "McDonnell Douglas DC-10", "Boeing 747", "Lockheed L-1011 TriStar", "Airbus 340"]
+	];
+
+
+//Pseudocode
+//Show title screen with a start button
+//Click start button to begin game
+//Show one question at a time
+//Set timer for 45 seconds
+//Player can choose only 1 answer to each question 
+//Show question until
+	//Player answers or
+	//Time runs out
+//Advance automatically to feedback screen after either event
+//Show feedback screen for 5 seconds
+	//if correct - say Congratulations! and show correct answer
+	//if incorrect - say Nope! and show correct answer
+	//if unanswered - say Time's up and show correct answer
+//On final screen show restart button and 
+	//number of correct answers
+	//wrong answers 
+	//unanswered questions
+
